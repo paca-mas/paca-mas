b370dfe187f765730389563a92072d4eaf3dc8b4
carlos.simon.garcia <carlos.simon.garcia@6fdbe20e-4a3f-0410-8ad4-79d6a75870c8>
2008-03-10 23:16:24 +0000

    Interfaz Swing: autentica, elige corrector, pide practicas, pide tests, pide ficheros y corrige practicas.

--- branches/paralelizacion-csimon/src/PACA/agents/lanzaSwing2.java
+++ branches/paralelizacion-csimon/src/PACA/agents/lanzaSwing2.java
@@ -8,6 +8,8 @@ import jade.wrapper.AgentController;
 import jade.wrapper.ContainerController;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.List;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
@@ -16,6 +18,7 @@ import javax.swing.JCheckBoxMenuItem;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 import PACA.util.*;
@@ -34,10 +37,7 @@ public class lanzaSwing2 {
 	static JComboBox cFicheros = null;
 	
 	
-	//Cosa rara
-	static JCheckBoxMenuItem listaTests = new JCheckBoxMenuItem();
-	
-	
+		
 	//JPanel ---- Uno por cada accion
 	static JPanel paraAutenticar = new JPanel();
 	static JPanel paraPracticas = new JPanel();
@@ -56,19 +56,16 @@ public class lanzaSwing2 {
 	static JTextField textoUsuario = new JTextField(10);
 	static JTextField textoPass = new JTextField(10);
 	
-	//Practicas
-	static String[] practicas = {"Practica1", "Practica2"};
-	static String[] practica1 = {"Test1", "Test2", "Test3"};
-	static String[] practica2 = {"TestA", "TestB", "TestC"};
-	static String[] practica3 = {"Test00", "Test01", "Test10"};
+	//Listas
+	static List listaTests = new List();
+	static List listaFichs = new List();
 	
-	static String[] Test1 = {"Fichero1", "Fichero2", "Fichero3", "Fichero4"};
-	static String[] Test2 = {"FicheroA", "FicheroB", "FicheroC", "FicheroD"};
-	static String[] Test3 = {"Fichero00", "Fichero01", "Fichero10", "Fichero11"};
+	//Salida correccion
+	static JTextArea correccion = new JTextArea("",5,20);
+	        
 	
-	
-	
-	
+	//Contenido fichero
+	static String contenido = "hola";
 	
 	static Runtime rt;
     static Profile p;
@@ -90,45 +87,23 @@ public class lanzaSwing2 {
 		
 		// TODO Auto-generated method stub
 		
-		//cPracticas = new JComboBox(practicas);
-		
+				
 		//---- Montar Paneles ----
-		textoUsuario.setSize(10, 2);
-		textoPass.setSize(10,2);
 		paraAutenticar.add(textoUsuario);
 		paraAutenticar.add(textoPass);
 		paraAutenticar.add(botonAutenticacion);
 		
-		//paraPracticas.add(cPracticas);
-		//paraPracticas.add(botonPracticas);
-		
-		
-		paraCorregir.add(botonCorregir);
-		
+			
 		//--- Fin Montar Paneles ----
+		
+		
 		
 		//--- Montar FRAME ----
 		
-		ventana.setSize(600, 300);
-		
+		ventana.setSize(600, 300);		
 		ventana.setDefaultCloseOperation(ventana.EXIT_ON_CLOSE);
-		
 		ventana.setLayout(new BorderLayout());
-		
 		ventana.getContentPane().add(paraAutenticar, BorderLayout.NORTH);
-		//ventana.getContentPane().add(paraPracticas, BorderLayout.WEST);
-		//ventana.getContentPane().add(paraTests, BorderLayout.CENTER);
-		//ventana.getContentPane().add(paraFicheros, BorderLayout.EAST);
-		ventana.getContentPane().add(paraCorregir, BorderLayout.SOUTH);
-		
-		
-		//ventana.getContentPane().setLayout(new GridLayout(10,100));
-		//ventana.getContentPane().add(paraAutenticar);
-		//ventana.getContentPane().add(paraPracticas);
-		//ventana.getContentPane().add(paraTests);
-		//ventana.getContentPane().add(paraFicheros);
-		//ventana.getContentPane().add(paraCorregir);
-		
 		ventana.setTitle("P A C A - Interfaz");
 		ventana.pack();
 		ventana.setVisible(true);
@@ -143,17 +118,9 @@ public class lanzaSwing2 {
 				System.out.println("usuario: "+texto);
 				System.out.println("pass: "+passw);
 				
-				
 				//--------- Arrancamos el agente ---------------------
-				/*
-				String nombre = passw+texto;
-				Runtime rt;
-			    Profile p;
-			    ContainerController cc;
-			    AgentController agentInterfaz=null;
-			    InterfazSwing2 agent=null;*/
-			       
-				String nombre = passw+texto;
+							       
+				String nombre = texto;
 				
 				try {
 						rt = Runtime.instance();
@@ -164,13 +131,9 @@ public class lanzaSwing2 {
 						if (agentInterfaz != null) {
 							agentInterfaz.start();
 							
-							//interfaz.setAgentInterfaz(agent);
-							//interfaz.setAgentController(agentInterfaz);
 							while (!agent.isFinSetup()) {
 								System.out.println("Esperando al fin... ");
 							}
-							
-							
 						} 
 						else {
 							System.out.println("Agente no Arrancado" + agentInterfaz.getState().getName());
@@ -187,43 +150,35 @@ public class lanzaSwing2 {
 				System.out.println("--------------------");
 								
 				while(!testigo.isRelleno()){
-					System.out.println("======");
-		  	 	}
+				}
 				
 				autenticado = testigo.isResultadoB();
 				System.out.println("autenticado: "+autenticado);
 				
 				if (autenticado){
-					Testigo testigo2 = new Testigo();
-					agent.swingPideCorrector(testigo2);
-					while(!testigo2.isRelleno()){
-						System.out.println("======");
-			  	 	}
-					System.out.println("==============================================");
-					System.out.println("==============================================");
+					testigo = new Testigo();
+					agent.swingPideCorrector(testigo);
+					while(!testigo.isRelleno()){
+					}
+										
+					testigo = new Testigo();
 					
-					Testigo testigo3 = new Testigo();
+					agent.swingPidePracticas(testigo);
+					while(!testigo.isRelleno()){
+					}
 					
-					agent.swingPidePracticas(testigo3);
-					while(!testigo3.isRelleno()){
-						System.out.println("======");
-			  	 	}
-					String [] pract = (String [])testigo3.getResultado();
+					String [] pract = (String [])testigo.getResultado();
 					System.out.println("PRACTICAS: "+pract.toString());
 					
 					cPracticas = new JComboBox(pract);
 					paraPracticas.add(cPracticas);
 					paraPracticas.add(botonPracticas);
-					
 					ventana.getContentPane().add(paraPracticas, BorderLayout.WEST);
+					
 				}
 				
-							
 				//----- FIN ARRANCAR AGENTE -----
-				
-				
-				
-				
+											
 			}
 		} );
 		
@@ -234,28 +189,13 @@ public class lanzaSwing2 {
 				//la propiedad getselecteditem() regresa un objeto
 				String practica = (String) cPracticas.getSelectedItem();
 				System.out.println("salida: "+practica+"**");
-				/*				
-				if (salida.equals("Practica1")){
-					cTests = new JComboBox(practica1);
-										
-				}
-				else{
-					if (salida.equals("Practica2")){
-						cTests = new JComboBox(practica2);
-						
-					}
-					else {
-						cTests = new JComboBox(practica3);
-						
-					}
-				}*/
-				Testigo testigo4 = new Testigo();
-				agent.swingPideTests(testigo4, practica);
-				while(!testigo4.isRelleno()){
-					System.out.println("======");
-		  	 	}
 				
-				String [] tests = (String [])testigo4.getResultado();
+				Testigo testigo = new Testigo();
+				agent.swingPideTests(testigo, practica);
+				while(!testigo.isRelleno()){
+				}
+				
+				String [] tests = (String [])testigo.getResultado();
 				
 				int tamano = tests.length / 2;
 				
@@ -264,16 +204,15 @@ public class lanzaSwing2 {
 						tests2[i]=tests[i*2];
 				}
 				
-			
-				cTests = new JComboBox(tests2);
-								
-				paraTests.add(cTests);
+						
+				for (int i = 0; i < tests2.length; i++) {
+					listaTests.add(tests2[i]);						
+				}
+				listaTests.setMultipleMode(true);
+				paraTests.add(listaTests);
 				paraTests.add(botonTests);
-				//ventana.getContentPane().add(paraTests);
 				ventana.getContentPane().add(paraTests, BorderLayout.CENTER);
-				
-				
-				
+						
 			}
 		} );
 		
@@ -282,39 +221,50 @@ public class lanzaSwing2 {
 		botonTests.addMouseListener( new MouseAdapter(){
 			public void mousePressed(MouseEvent e){
 				//la propiedad getselecteditem() regresa un objeto
-				String [] ficheros = new String[1];
-				ficheros[0] = (String) cTests.getSelectedItem();
-				System.out.println("salida: "+ficheros+"**");
+				String [] testss  =  listaTests.getSelectedItems();
+				System.out.println("salida: "+testss+"**");
 				
-				/*
-				if (salida.equals("Practica1")){
-					cFicheros = new JComboBox(Test1);
-					
+								
+				Testigo testigo = new Testigo();
+				agent.swingPideFicheros(testigo, testss);
+				
+				while(!testigo.isRelleno()){
 				}
-				else{
-					if (salida.equals("Practica2")){
-						cFicheros = new JComboBox(Test2);
-					}
-					else {
-						cFicheros = new JComboBox(Test3);
-					}
-				}*/
 				
-				Testigo testigo5 = new Testigo();
-				agent.swingPideFicheros(testigo5, ficheros);
+				String [] fichs = (String [])testigo.getResultado();
 				
-				while(!testigo5.isRelleno()){
-					System.out.println("======");
-		  	 	}
-				
-				String [] fichs = (String [])testigo5.getResultado();
-				
-				cFicheros = new JComboBox(fichs);
-				paraFicheros.add(cFicheros);
+				for (int i = 0; i < fichs.length; i++) {
+					listaFichs.add(fichs[i]);
+				}
+				listaFichs.setMultipleMode(true);
+				paraFicheros.add(listaFichs);
 				paraFicheros.add(botonFicheros);
 				ventana.getContentPane().add(paraFicheros, BorderLayout.EAST);
 			}
 		} );
+		
+		
+		botonFicheros.addMouseListener( new MouseAdapter(){
+			public void mousePressed(MouseEvent e){
+				String [] ficheros = listaFichs.getSelectedItems();
+				Testigo testigo = new Testigo();
+				String [] cont = new String[ficheros.length];
+				for (int i = 0; i < cont.length; i++) {
+					cont[i]=contenido;
+				}
+				agent.swingPideCorreccion(testigo, cont);
+				while(!testigo.isRelleno()){
+				}
+				
+				String salida = (String) testigo.getResultado();
+				
+				correccion.setText(salida);
+				paraCorregir.add(correccion);
+				paraCorregir.add(botonCorregir);
+				ventana.getContentPane().add(paraCorregir, BorderLayout.SOUTH);
+				
+			}
+		});
 		
 				
 	}
