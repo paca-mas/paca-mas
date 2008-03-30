fc56a41ac0b52c65b673341d3a79465ae351e7ac
carlos.simon.garcia <carlos.simon.garcia@6fdbe20e-4a3f-0410-8ad4-79d6a75870c8>
2008-03-30 18:43:48 +0000

    SimulaAgentes: Ahora es un Runnable. Interfaz: Añadidas capturas de excepciones. lanzador: Cambio necesario para lanzar hilos.

--- branches/paralelizacion-csimon/src/PACA/agents/lanzador.java
+++ branches/paralelizacion-csimon/src/PACA/agents/lanzador.java
@@ -4,6 +4,9 @@ import java.awt.Dimension;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
@@ -16,9 +19,18 @@ public class lanzador {
 	static JFrame ventana = new JFrame();
 	static JPanel panel = new JPanel();
 	static JComboBox caja = new JComboBox();
-	static int numero = 10;
+	static int numero = 20;
 	static JButton botonLanza = new JButton("Numero de Agentes"); 
 	static boolean terminado = false;
+	
+	static void GeneraRetardo(int ret){
+		try {
+			Thread.sleep(ret);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 	
 	public static void main(String[] args) {
 		for (int i = 1; i <= numero; i++) {
@@ -39,6 +51,8 @@ public class lanzador {
 		
 		
 		
+		
+		
 		//Selecciona el numero de Agentes a lanzar
 		botonLanza.addMouseListener( new MouseAdapter(){
 				public void mousePressed(MouseEvent e){
@@ -46,34 +60,65 @@ public class lanzador {
 				String numero  =  (String) caja.getSelectedItem();
 				System.out.println("Agentes para lanzar: "+numero);
 				
-				for (int i = 1; i <= Integer.valueOf(numero); i++) {
-					try {
+				Thread[] hilos = new Thread[Integer.valueOf(numero)];
+				
+				Thread t1;
+				
+				for (int i = 0; i < Integer.valueOf(numero); i++) {
+					//try {
 						Runtime rt = Runtime.getRuntime();
 						System.out.println("Intentamos lanzar el comando");
 						//rt.exec("C:\\Documents and Settings\\Carlos\\Escritorio\\lanzaAgentes.bat");
-						rt.exec("java PACA.agents.lanzaSwing2");
-						//rt.exec("java PACA.agents.SimulaAgentes");
-						System.out.println("Se supone que se lanza");
+						//rt.exec("java PACA.agents.lanzaSwing2");
 						//try {
-							//Thread.sleep(200);
-						//} 
+							//rt.exec("java PACA.agents.SimulaAgentes");
+							//rt.exec("java PACA.agents.lanzaSwing2");
+						//} catch (IOException e1) {
+							// TODO Auto-generated catch block
+							//e1.printStackTrace();
+						//}
+						
+						t1 = new Thread(new SimulaAgentes());
+						//new Thread(new SimulaAgentes()).start();
+						//SimulaAgentes sim1 = new SimulaAgentes();
+						//pool.execute(sim1);
+						//t1 = new SimulaAgentes();
+												
+						//t1.start();
+						hilos[i] = t1;
+						
+						//GeneraRetardo(1000);
+						//try {
+							//Thread.sleep(2000);
+							//System.out.println("Se supone que se lanza");
+						//}
+						
 						//catch (InterruptedException e1) {
 							// TODO Auto-generated catch block
 							//e1.printStackTrace();
 						//}
 					
-					} catch (IOException e1) {
+					//} catch (IOException e1) {
 						// TODO Auto-generated catch block
-						e1.printStackTrace();
-					}		
+						//e1.printStackTrace();
+					//}		
+				}
+				
+				System.out.println("Lanzamos!!!");
+				
+				
+				for (int i = 0; i < hilos.length; i++) {
+					hilos[i].start();
+					GeneraRetardo(2000);
+					
+					System.out.println("Lanzamos!!!!"+((Thread) hilos[i]).getName());
 				}
 				
 				terminado = true;
 				
 				if (terminado){
 					try{
-						System.exit(0);
-						//hebra.exit(0);
+						//System.exit(0);
 					}
 					catch (Exception e2) {
 						// TODO Auto-generated catch block
