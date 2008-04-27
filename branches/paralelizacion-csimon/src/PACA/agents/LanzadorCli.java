cd5eacda4d4ae675679705d7ca28b2641eda9309
carlos.simon.garcia <carlos.simon.garcia@6fdbe20e-4a3f-0410-8ad4-79d6a75870c8>
2008-04-27 21:41:35 +0000

    LanzadorCli: Nuevo lanzador por línea de comandos (los argumentos son política, número de agentes, número de correcciones, práctica elegida, test elegido y tamaño de los ficheros) SimulaAgentes: Cambiado el constructor para pasar todos los parametros del lanzador. lanzador: Cambio en la estructura. Añadida caja para número de correciones

---
+++ branches/paralelizacion-csimon/src/PACA/agents/LanzadorCli.java
@@ -0,0 +1,61 @@
+package PACA.agents;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+import jade.core.Profile;
+import jade.core.ProfileImpl;
+import jade.core.Runtime;
+import jade.wrapper.ContainerController;
+
+public class LanzadorCli {
+	
+		
+	public static void GeneraRetardo(int ret){
+		try {
+			Thread.sleep(ret);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public static void main( String args[] ) {
+		Runtime rt = Runtime.instance();
+		Profile p = new ProfileImpl(false);
+		ContainerController cc = rt.createAgentContainer(p);
+		for( int i=0; i < args.length; i++ ){
+			System.out.println( args[i] );
+		}	
+		
+		String politica = args[0];
+		String numero  = args[1];
+		String nCorreciones = args[2];
+		String practica = args[3];
+		String tests = args[4];
+		String tamano = args[5];
+		
+		int numThreads = Integer.valueOf(numero);
+		int numeroCorreciones = Integer.valueOf(nCorreciones);
+		int totalCorrec = numeroCorreciones / numThreads;
+		int npract = Integer.valueOf(practica);
+		int ntests = Integer.valueOf(tests);
+		int ntamano = Integer.valueOf(tamano);
+		
+		Executor pool = Executors.newFixedThreadPool(numThreads);
+		
+		for (int i = 0; i < numThreads; i++) {
+							
+			
+			//------------------ NO BORRAR -----------------
+			SimulaAgentes sim1 = new SimulaAgentes(cc, i, politica, totalCorrec, npract,
+													ntests, ntamano);
+			pool.execute(sim1);
+			//------------------ FIN NO BORRAR -------------
+							
+			GeneraRetardo(150);
+
+		}
+	}
+}  
+
+
