ae72ec7f61664700af14c04fffa5111810847140
sandrita.84 <sandrita.84@6fdbe20e-4a3f-0410-8ad4-79d6a75870c8>
2010-05-01 08:42:50 +0000

    Corrector Prueba, random

--- src/es/urjc/ia/paca/agents/CorrectorPrueba.java
+++ src/es/urjc/ia/paca/agents/CorrectorPrueba.java
@@ -3,7 +3,7 @@ package es.urjc.ia.paca.agents;
 
 import jade.core.behaviours.*;
 import jade.lang.acl.ACLMessage;
- 
+import java.util.*; 
 import jade.content.*;
 import jade.content.lang.*;
 import jade.content.lang.leap.*;
@@ -32,8 +32,9 @@ public class CorrectorPrueba extends Agent {
 		public boolean done() { return true; }
  
 		public void action() {
-			try {    
-// request + remitente + agente receptor + lenguaje + protocolo + contenido				
+			try {
+				for (int i= 0; i <10; i++){ 
+					// request + remitente + agente receptor + lenguaje + protocolo + contenido				
 					ACLMessage mensaje = new ACLMessage(ACLMessage.REQUEST);	// Enviamos un mensaje REQUEST
 					mensaje.setSender(getAID());	// Remitente
 					AID receptor_msg = new AID(nombre,AID.ISLOCALNAME);	// Agente al que le enviamos el mensaje
@@ -41,15 +42,20 @@ public class CorrectorPrueba extends Agent {
 					mensaje.setLanguage(codec.getName());	// Lenguaje
 					mensaje.setProtocol(FIPANames.InteractionProtocol.FIPA_REQUEST);
 					
+					Random rand = new Random();
+					int caso = rand.nextInt(100);
+					int test = rand.nextInt(100);
+					int practica = rand.nextInt(100);
+						
 					EvaluacionCaso evaluacioncaso = new EvaluacionCaso();
 					Alumno a = new Alumno ();
 					a.setIdentificador("1610");
 					Caso c = new Caso ();
-					c.setId("2");
+					c.setId(Integer.toString(caso));
 					Test t = new Test ();
-					t.setId("4");
+					t.setId(Integer.toString(test));
 					Practica p = new Practica ();
-					p.setId("1");
+					p.setId(Integer.toString(practica));
 					
 					evaluacioncaso.setAlumno(a);
 					evaluacioncaso.setPractica(p);
@@ -65,12 +71,10 @@ public class CorrectorPrueba extends Agent {
 					send(mensaje);	// enviamos el mensaje
 				
 				System.out.println( "[" + getLocalName() + "] El mensaje ha sido creado y enviado.");
-				
-			}
-			
-			catch(Exception excep) { excep.printStackTrace(); }
+				Thread.sleep (10000);
+				}
+			} catch (Exception e) { e.printStackTrace();	}				
 		}
-
 	}
 
 	
