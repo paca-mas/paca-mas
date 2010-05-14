729121a1c8f5f4b70c40292bfa6ad66f7d200fee
sandrita.84 <sandrita.84@6fdbe20e-4a3f-0410-8ad4-79d6a75870c8>
2010-05-14 17:22:03 +0000

    Interfaz Agente

---
+++ branches/gestos-estadisticas/src/es/urjc/ia/paca/util/AgentBeanGestorEstadistica.java
@@ -0,0 +1,59 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package es.urjc.ia.paca.util;
+
+import jade.wrapper.AgentController;
+import es.urjc.ia.paca.agents.InterfazJSPGestorEstadistica;
+
+public class AgentBeanGestorEstadistica {
+
+	private InterfazJSPGestorEstadistica agente;
+	private AgentController Atributo;
+
+	public AgentBeanGestorEstadistica() {
+		super();
+	}
+
+	public void wait4Agent() {
+
+		while (this.agente == null) {
+		//System.out.println("ESPERA ACTIVA... ");
+		}
+	}
+
+	public void setAgentInterfaz(InterfazJSPGestorEstadistica ag) {
+		this.agente = ag;
+	}
+
+	public InterfazJSPGestorEstadistica getAgentInterfaz() {
+		wait4Agent();
+		return this.agente;
+	}
+
+
+	public void sendTestigo(Testigo testigo) {
+		wait4Agent();
+		this.agente.sendTestigo(testigo);
+	}
+
+	public void setAlumnoID(String id) {
+		wait4Agent();
+		this.agente.setAlumnoID(id);
+	}
+
+	public void setAlumnoPass(String pass) {
+		wait4Agent();
+		this.agente.setAlumnoPass(pass);
+	}
+
+	public AgentController getAgentController() {
+		return Atributo;
+	}
+
+	public void setAgentController(AgentController atributo) {
+		Atributo = atributo;
+	}
+}
